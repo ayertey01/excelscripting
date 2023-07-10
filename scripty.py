c3e203a40dcb60d8e1a9a78a23e9c51d0419b957
ayertey01 <pyterayertey@gmail.com>
2023-07-10 22:23:51 +0000

    push

---
+++ scripty.py
@@ -0,0 +1,48 @@
+import openpyxl
+from openpyxl import load_workbook
+#from openpyxl import save 
+
+inv_file=load_workbook("inventory.xlsx")
+product_list=inv_file["Sheet1"]
+
+
+products_per_supplier ={} 
+total_value_per_supplier={}
+product_under_10_inv ={}
+
+for product_row in range(2,product_list.max_row + 1):
+    supplier_name=product_list.cell(product_row,4).value
+    inventory = product_list.cell(product_row,2).value
+    price=product_list.cell(product_row,3).value
+    product_num = product_list.cell(product_row,1).value
+    inventory_price = product_list.cell(product_row,5)
+    #print(supplier_name)
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier[supplier_name]
+        products_per_supplier[supplier_name]=current_num_products +1
+    else: 
+        print("adding new supplier")
+        products_per_supplier[supplier_name] = 1
+
+
+#calculation total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier[supplier_name]
+        total_value_per_supplier[supplier_name]= current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name]= inventory * price
+
+
+#calculations of products vs inventory
+    if inventory < 10: 
+        product_under_10_inv[product_num]=inventory
+#print(product_under_10_inv)
+
+#adding inventory value 
+    inventory_price.value = inventory * price
+print(products_per_supplier)
+print(current_total_value)
+print(product_under_10_inv)
+
+
+inv_file.save("inventory_with_total_value.xlsx")
